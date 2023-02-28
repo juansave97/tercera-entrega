cc89ecd58aa4cc565c9cf8cfe38666b775c07bba
Julian Salazar <juansave97@gmail.com>
2023-02-28 18:13:19 +0100

    tercera entrega

---
+++ daos/carritos/CarritoDAOMongoDB.js
@@ -0,0 +1,16 @@
+const mongoDB = require(`../../dataBase/options/mongoDB`);
+
+const carritoModel = require(`../../dataBase/models/carrito`);
+const productsModel = require(`../../dataBase/models/producto`);
+const userModel = require(`../../dataBase/models/user`);
+
+const CrudMongoDB = require(`../../dataBase/crudMongoDB/crudCarritos`);
+
+class CarritoDAOMongoDB extends CrudMongoDB {
+    constructor() {
+        super(mongoDB, carritoModel, productsModel, userModel);
+    };
+};
+
+module.exports = CarritoDAOMongoDB;
+
